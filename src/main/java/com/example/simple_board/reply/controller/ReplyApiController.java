09a69470af94971410fb4eb236f2ceadbe9e1aab
Ysw <202021079@sangmyung.kr>
2025-02-15 23:24:13 +0900

    추상화를 통한 게시판 프로젝트 개선 - 4

--- src/main/java/com/example/simple_board/reply/controller/ReplyApiController.java
+++ src/main/java/com/example/simple_board/reply/controller/ReplyApiController.java
@@ -1,7 +1,9 @@
 package com.example.simple_board.reply.controller;
 
 
+import com.example.simple_board.crud.CRUDAbstractApiController;
 import com.example.simple_board.reply.db.ReplyEntity;
+import com.example.simple_board.reply.model.ReplyDto;
 import com.example.simple_board.reply.model.ReplyRequest;
 import com.example.simple_board.reply.service.ReplyService;
 import jakarta.validation.Valid;
@@ -14,16 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/reply")
 @RequiredArgsConstructor
-public class ReplyApiController {
+public class ReplyApiController extends CRUDAbstractApiController<ReplyDto, ReplyEntity> {
 
-    private final ReplyService replyService;
-
-    @PostMapping("")
-    public ReplyEntity create(
-            @Valid
-            @RequestBody ReplyRequest replyRequest
-    ){
-        return replyService.create(replyRequest);
-    }
+//    private final ReplyService replyService;
+//
+//    @PostMapping("")
+//    public ReplyEntity create(
+//            @Valid
+//            @RequestBody ReplyRequest replyRequest
+//    ){
+//        return replyService.create(replyRequest);
+//    }
 
 }
